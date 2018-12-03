4fbffda45497e7890592766eda121145c976f6bd
Kevin Le <captianle@gmail.com>
2018-12-03 14:49:20 -0800

    3rd hotfix

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+require('dotenv').config();
+
 var app = express();
 
 app.use(logger('dev'));
